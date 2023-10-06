d88a911e3d60a8795d934df388d0c169b3efd0d8
Yashlin Naidoo <yashycr7@gmail.com>
2023-10-06 12:51:03 +0200

    route guard

--- BaseUi/src/app/app-routing.module.ts
+++ BaseUi/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeGuard } from './home.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent,canActivate: [HomeGuard] } // base route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ BaseUi/src/app/home.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeGuard implements CanActivate {
+  // Mock user authentication status
+  userIsAuthenticated = true; // Change this to false to see the guard in action
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.userIsAuthenticated;
+  }
+
+}
